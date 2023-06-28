2ef8290f7f198227d874bc226e1134404afbb339
valeria caramellino <valeriacaramellino@gmail.com>
2023-06-28 12:08:59 +0200

    perfetto pagina home

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,9 @@
             <!--sezione blu non riesco a ciclare su img passando $icon['icon']-->
             @foreach($footerBlue as $icon)
                 <section class="d-flex align-items-center col-2 p-2">
-                    <img class="col-2" src= "{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="icon">
+                    
+                    <img class="col-2" src= "{{ Vite::asset($icon['icon']) }}" alt="icon">
+                    
                     <p class="col mt-3">{{ $icon['text'] }}</p>
                 </section>
             @endforeach
